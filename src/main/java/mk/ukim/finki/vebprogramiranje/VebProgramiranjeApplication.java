c2e5eed6b3555712407fed708ba70fc17691e508
QuarantineAbuser <stefanprilep7@gmail.com>
2021-10-23 15:00:58 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/vebprogramiranje/VebProgramiranjeApplication.java
@@ -0,0 +1,14 @@
+package mk.ukim.finki.vebprogramiranje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class VebProgramiranjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VebProgramiranjeApplication.class, args);
+    }
+}
---
+++ src/test/java/mk/ukim/finki/vebprogramiranje/VebProgramiranjeApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.vebprogramiranje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VebProgramiranjeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
